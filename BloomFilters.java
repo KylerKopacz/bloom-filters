fe1e86f94cb2552410319e0d3f3aa0e4fb292cf6
Kyler Kopacz <kylerkopacz@gmail.com>
2019-05-15 22:15:08 -0400

    Cleaned up run.sh. Documented and reformatted code.

--- BloomFilters.java
+++ BloomFilters.java
@@ -1,86 +1,156 @@
-//************************************** */
-//******** BLOOM FILTERS **************/
-// Kyler Kopacz, Jack Fergus, and Dylan Finazzo
-//************************************** */
+/** 
+* Bloom Filter Simulation
+* @author Dylan Finazzo
+* @author Kyler Kopacz
+* @author Jack Fergus
+*/
 import java.util.*;
-//************************************** */
+
+/**
+* Object Representation of a Bloom Filter.
+* The main method is also the runner for the experiment.
+*/
 public class BloomFilters {
-    //************************************** */
-    /* FIELDS */
-    //************************************** */
+    /** Number of insertions to perform */
     static int num_insertions = 1000;
+    
+    /** Range for numbers to be inserted */
     static int insertion_range = 100000;
-    int[] A; //the bit Array
-    int k; // number of hash functions
-    int size; // size of the bit array
-    HashSet actual; //Set of elements actually added to filter
-
-    public BloomFilters(int num, int s){
+    
+    /** The "bitset". */
+    int[] A;
+    
+    /** Number of hash functions being used. */
+    int k; 
+    
+    /** Size of the bitset. */
+    int size;
+    
+    /** Hash set of all numbers that are actually added to the filter. */
+    HashSet<Integer> actual;
+    
+    /**
+    * Constructor to make a Bloom Filter Object.
+    * @param num Number of hash functions for this filter.
+    * @param size Size of the bitset for the filter.
+    */
+    public BloomFilters(int num, int size){
         k = num;
-        A = new int[s];
-        size = s;
-        actual = new HashSet();
+        this.size = size;
+        A = new int[size];
+        actual = new HashSet<Integer>();
     }
-    //************************************** */
+    
+    /**
+    * Insert an element into the bloom filter.
+    * @param n The element to be inserted.
+    */
+    public void insert(int n) {
+        
+        // Add the inserted element to a hashset for false positive checking
+        actual.add(n); 
+        
+        //Generate random generator with a seed of the number inserted
+        Random rand = new Random(n); 
+        
+        // for k hash functions, generate random numbers within the range of our "bit" array (acts as hash function)
+        for(int i=0; i<k; i++) {
 
+            //generate index
+            int index = rand.nextInt(size); 
+
+            //set that "bit" to 1
+            A[index] = 1; 
+        }
+    }
+    
+    /**
+     * Lookup an element in the bloom filter.
+     * @param n The element to look up.
+     * @return Whether or not the specified element was found.
+     */
+    public boolean lookup(int n){
+
+        //Random generator with the same seed will return same sequence of numbers
+        Random rand = new Random(n); 
+        
+        for(int i=0; i<k; i++) {
+            //Corresponds to the indices of the bit array that would be set if the element was inserted, based on number of hashes
+            int index = rand.nextInt(size); 
+            
+            //If any of the bits are 0 the element was not added to the filter
+            if(A[index]==0) { 
+                //immediately return false
+                return false;               
+            }
+        }
+        return true;
+    }
+    
+    /**
+     * Returns the false positive rate of the bloom filter.
+     * @param num Number of elements to query.
+     * @return The false positive rate.
+     */
+    public double fpRate(int num){
+        //False positives + true negatives
+        int total_negatives = 0; 
+        
+        //false positives
+        int fp = 0; 
+        
+        Random rand = new Random();
+        
+        for(int i=0; i<num; i++) {
+            //Randomly generate ints that were not inserted
+            int trial = insertion_range + rand.nextInt(insertion_range);
+            
+            if(!actual.contains(trial)) { 
+                // If the trial element was not inserted, increment the total negatives
+                total_negatives++;
+                
+                //If the filter says that the element is in the set then increment the number of false positives
+                if(lookup(trial)) { 
+                    fp++;
+                }
+            }
+        }
+        //False positive rate
+        return (double)fp/(double)total_negatives; 
+    }
+
+    /**
+     * Shows the correct usage for the program, and exits.
+     */
+    public static void showUsageAndExit() {
+        System.out.println("Incorrect usage: ./BloomFilters <number of hash functions> <size of bit array>");
+        System.exit(1);
+    }
+    
+    /**
+     * Runner for one iteration of the experiment.
+     * @param args Arguments to be passed for experimentation.
+     */
     public static void main(String[] args){
+        //make sure we have all the arguments necessary
+        if(args.length != 2) {
+            showUsageAndExit();
+        }
 
         int k = Integer.parseInt(args[0]); // Number of hash functions
         int size = Integer.parseInt(args[1]); // Size of the bit array
         
         BloomFilters filter = new BloomFilters(k, size);
         Random rand = new Random();
-
-        for(int i=0; i<num_insertions; i++){
+        
+        for(int i=0; i<num_insertions; i++) {
+            //generate a number to insert between 0 and insertion_range
             int x = rand.nextInt(insertion_range); 
-            filter.insert(x); //Insert random numbers between 0 and insertion_range
+
+            //Insert element
+            filter.insert(x); 
         }
         System.out.println(filter.fpRate(1000));
-    }
-
-    public void insert(int n){
-
-        actual.add(n); // Add the inserted element to a hashset for false positive checking
-
-        Random rand = new Random(n); //Generate random generator with a seed of the number inserted
-        // for k hash functions, generate random numbers within the range of our "bit" array (acts as hash function)
-        for(int i=0; i<k; i++){
-            int index = rand.nextInt(size); 
-            A[index] = 1; //set bits to 1
-        }
-    }
-
-    public boolean lookup(int n){
-    
-        boolean toReturn = true;
-        Random rand = new Random(n); //Random generator with the same seed will return same sequence of numbers
-        
-        for(int i=0; i<k; i++){
-            int index = rand.nextInt(size); //Corresponds to the indices of the bit array that would
-            if(A[index]==0){                //be set if the element was inserted, based on number of hashes
-                toReturn = false; //If any of the bits are 0 the element was not added to the filter
-            }
-        }
-        return toReturn;
-    }
-
-    public double fpRate(int num){
-
-        int total_negatives = 0; //False positives + true negatives
-        int fp = 0; //false positives
-
-        Random rand = new Random();
-
-        for(int i=0; i<num; i++){
-            int trial = insertion_range + rand.nextInt(insertion_range); //Randomly generate ints that were not inserted
-            if(!actual.contains(trial)){ // If the trial element was not inserted
-                total_negatives++;
-                if(lookup(trial)){ //If the filter says that the element is in the set
-                    fp++;
-                }
-            }
-        }
-        return (double)fp/(double)total_negatives; //False positive rate
     }
 }
 
--- run.sh
+++ run.sh
@@ -2,6 +2,14 @@
 
 javac *.java
 
+mkdir -p output
+
+echo "Starting Bloom Filter Experimentation..."
+echo "NOTE: The contents of the output is the false positive rates, given various parameters.
+"
+
+echo "Starting experimentation with 3 hash functions and varying sizes of bit sets..."
+
 java BloomFilters 3 10000 >> output/output3hashes.txt
 java BloomFilters 3 20000 >> output/output3hashes.txt
 java BloomFilters 3 30000 >> output/output3hashes.txt
@@ -13,10 +21,15 @@ java BloomFilters 3 80000 >> output/output3hashes.txt
 java BloomFilters 3 90000 >> output/output3hashes.txt
 java BloomFilters 3 100000 >> output/output3hashes.txt
 
+echo "Done! check the \"output\" directory for results.
+"  
+
+echo "Now starting experimentation with varying number of hash sizes, and varying sizes of bit sets..."
+
 for ((i=1;i<=10;i++))
 do
 
-osascript -e 'display notification "Starting Round" with title "Bloom Filter Status"'
+echo "Starting round $i..."
 
 java BloomFilters $i 10000 >> output/output10000.txt
 java BloomFilters $i 25000 >> output/output25000.txt
@@ -24,6 +37,8 @@ java BloomFilters $i 50000 >> output/output50000.txt
 java BloomFilters $i 100000 >> output/output100000.txt
 java BloomFilters $i 200000 >> output/output200000.txt
 
-osascript -e 'display notification "Done with Round" with title "Bloom Filter Status"'
-osascript -e 'say "Congratulations!"'
+echo "Finished with round $i."
+
 done
+
+echo "Done! check the \"output\" directory for results."
